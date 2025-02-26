a71acd44f5798d18980b9d175ca26f42358bdfe7
joel-cagla <joel@cagla.jp>
2025-02-26 16:47:03 +0900

    Initial Commit

---
+++ Source Control/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Source Control
+//
+//  Created by Joel Tron on 2025/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Rectangle().fill(Color.green)
+                .ignoresSafeArea()
+                .navigationTitle("source control")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Source Control/Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_ControlApp.swift
+//  Source Control
+//
+//  Created by Joel Tron on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
